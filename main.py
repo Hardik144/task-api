0c696aa827667bd7f6e97b7e929c22bfab5a72a5
Hardik144 <patidarhardik81@gmail.com>
2026-07-17 17:16:42 +0530

    Stage 4: implement PUT and DELETE endpoints

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ class TaskCreate(BaseModel):
     completed: bool = False
 
 
-# Temporary in-memory database
 tasks = [
     {
         "id": 1,
@@ -62,3 +61,24 @@ def create_task(task: TaskCreate):
 
     tasks.append(new_task)
     return new_task
+
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: TaskCreate):
+    for task in tasks:
+        if task["id"] == task_id:
+            task["title"] = updated_task.title
+            task["completed"] = updated_task.completed
+            return task
+
+    raise HTTPException(status_code=404, detail="Task not found")
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return {"message": "Task deleted successfully"}
+
+    raise HTTPException(status_code=404, detail="Task not found")
